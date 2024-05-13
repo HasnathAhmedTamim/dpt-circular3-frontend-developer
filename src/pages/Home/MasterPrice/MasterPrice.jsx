095acd091206236ecaae43aeccf17e7769c8053d
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-05-13 21:48:38 +0600

    live link set

--- src/pages/Home/MasterPrice/MasterPrice.jsx
+++ src/pages/Home/MasterPrice/MasterPrice.jsx
@@ -17,7 +17,9 @@ const MasterPrice = () => {
     // Fetch data if search is triggered
     if (isSearchData) {
       axios
-        .get("http://localhost:5173/data/LHR_CDG_ADT1_TYPE_1.txt")
+        .get(
+          "https://dpt-circular3-frontend-developer-eta.vercel.app//data/LHR_CDG_ADT1_TYPE_1.txt"
+        )
         .then((resp) => setMyData(resp?.data?.flightOffer || []))
         .catch((error) => console.error("Error fetching data:", error));
     }
